716ea2b0b133789235ba40fc118e74f70c9c9f91
Jeevalakshmi94 <76044776+Jeevalakshmi94@users.noreply.github.com>
2021-12-25 20:39:30 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,13 +11,20 @@ var firebaseConfig = {
     
     // Initialize Firebase
      firebase.initializeApp(firebaseConfig);
-   username=localStorage.getItem("username")
-document.getElementById("welcome").innerHTML="welcome "+username
-;
-var roomname=document.getElementById("room_name").value
-
-function addroom() {
-  localStorage.setItem("room_name",roomname)
-  firebase.database().ref("/").child(roomname).update({ purpose:"adding room name" });
-  window.location="kwitter_page.html"
+  user_name=localStorage.getItem("user_name")
+document.getElementById("user_name").innerHTML="Welcome"+user_name+"!"
+function addRoom(){
+room_name=document.getElementById("room_name").value;
+firebase.database().ref("/").child(room_name).update({
+purpose:"adding room name"
+});
+localStorage.setItem("room_name", room_name);
+window.location="kwitter_page.html"
 }
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
